8adea4f60cd38986434cb1c647ce23aa227c9be2
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-26 15:21:41 +0530

    add fav and remove fav functin added to Context

--- RNMeals/Store/Context/Favourite-Context.js
+++ RNMeals/Store/Context/Favourite-Context.js
@@ -1,14 +1,27 @@
-import React, {createContext} from 'react';
+import React, {createContext, useState} from 'react';
 
 export const FavoritesContext = createContext({
-    id: [],
-    addFavorites: ()=>{},
-    removeFavorites: ()=>{},
-})
+  id: [],
+  addFavorites: () => {},
+  removeFavorites: () => {},
+});
 
 function FavoritesContextProvider({children}) {
-    return <FavoritesContext.Provider>
-        {children}
-    </FavoritesContext.Provider>
+  const [favoriteMealId, setAddFavoriteMeal] = useState([]);
+  function addFavorite(id) {
+    setAddFavoriteMeal(cuurentFavIds => [...cuurentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setAddFavoriteMeal(currentFavId =>
+      currentFavId.filter(mealId => mealId != id),
+    );
+  }
+  const value = {
+    ids: [],
+    addFavorites: addFavorite,
+    removeFavorites: removeFavorite,
+  };
+  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
 }
 export default FavoritesContextProvider;
