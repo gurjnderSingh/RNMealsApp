5f0825907d513308d6a6e3bffa4ccd8fa3e761dd
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-27 17:18:49 +0530

    message added when there is not any favorite

--- RNMeals/screens/FavouriteScreen.js
+++ RNMeals/screens/FavouriteScreen.js
@@ -1,12 +1,33 @@
 import React, {useContext, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {FavoritesContext} from '../Store/Context/Favourite-Context';
 import {MEALS} from '../data/dummy-data';
 import MealList from '../components/MealList/MealList';
+import {color} from 'react-native-reanimated';
 
 function FavouriteScreen() {
   const favoriteContext = useContext(FavoritesContext);
   const favMeals = MEALS.filter(meal => favoriteContext.ids.includes(meal.id));
+  if (favMeals.length == 0) {
+    return (
+      <View style={style.rootContiner}>
+        <Text style={style.text}>You have not any favorite yet.</Text>
+      </View>
+    )
+  }
   return <MealList items={favMeals} />;
 }
 
 export default FavouriteScreen;
+const style = StyleSheet.create({
+  rootContiner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+    color: 'blue',
+  },
+});
